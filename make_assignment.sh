7fe006213d6927d606516d046e8f531fe2f5f7ca
Alan Collins <alan.collins@bath.edu>
2025-07-04 15:23:17 -0400

    fix variable name

--- make_assignment.sh
+++ make_assignment.sh
@@ -9,10 +9,10 @@ rm $GITHUB_BRANCH.zip
 mkdir -p zip_$GITHUB_BRANCH
 cp gradescope_base/* zip_$GITHUB_BRANCH/
 
-sed -i "s,REPLACE_GITHUB_BRANCH,GITHUB_BRANCH=$GITHUB_BRANCH," zip_$name/setup.sh
+sed -i "s,REPLACE_GITHUB_BRANCH,GITHUB_BRANCH=$GITHUB_BRANCH," zip_$GITHUB_BRANCH/setup.sh
 
 # zip the assignement and delete folder
-zip -r -m -j $name.zip zip_$name/*
-rmdir zip_$name
+zip -r -m -j $GITHUB_BRANCH.zip zip_$GITHUB_BRANCH/*
+rmdir zip_$GITHUB_BRANCH
 
 
