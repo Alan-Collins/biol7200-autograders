0079593c6a099f0f4455bede0560ca8c1f9a9b61
Alan Collins <alan.collins@bath.edu>
2025-07-05 20:05:31 -0400

    show issue stdout

--- tests/test_dir.py
+++ tests/test_dir.py
@@ -143,7 +143,8 @@ class TestFiles(unittest.TestCase):
         if TEST_PATH != self._stdout.strip():
             self.fail(
                 "Your script should only print the absolute path of the new working directory to stdout."
-                    "There should be nothing else written to the stdout."
+                "There should be nothing else written to the stdout. "
+                f"Your script printed{self._stdout.strip()}"
             )
         print("The stdout of your script only contains the instructed contents: the path of your scripts working directory.")
     
