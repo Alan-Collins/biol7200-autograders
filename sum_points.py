b37ec015f1cb6e4348bb2b168b6f613afe503642
Alan Collins <alan.collins@bath.edu>
2025-07-08 18:29:57 -0400

    only sum points in test files

--- sum_points.py
+++ sum_points.py
@@ -4,6 +4,8 @@ import os
 import re
 
 for file in os.listdir("tests/"):
+    if not file.startswith("test"):
+        continue
     with open(f"tests/{file}") as fin:
         vals = [int(i) for i in re.findall(r"@weight\((\d+)\)",fin.read())]
     print(f"{file}\t{sum(vals)}")
