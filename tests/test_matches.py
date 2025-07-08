5006f090d544b09b4f0ce9b457f10f0260c51116
Alan Collins <alan.collins@bath.edu>
2025-07-08 18:40:15 -0400

    debugging

--- tests/test_matches.py
+++ tests/test_matches.py
@@ -238,6 +238,7 @@ class TestFiles(unittest.TestCase):
 
         with open(self._match_out) as f:
             num_lines = len(f.readlines())
+            print(num_lines)
         if num_lines == 0:
             self.fail("Your script produced an empty output file.")
         
@@ -246,6 +247,7 @@ class TestFiles(unittest.TestCase):
 
         with open(self._no_match_out) as f:
             num_lines = len(f.readlines())
+            print(num_lines)
         if num_lines == 0:
             self.fail("Your script's output file contains the wrong number of matches.")
         
@@ -263,6 +265,8 @@ class TestFiles(unittest.TestCase):
         """Check correct number of matches in stdout"""
         match_numbers = re.findall("\d+", self._match_stdout)
         no_match_numbers = re.findall("\d+", self._no_match_stdout)
+        print(match_numbers, no_match_numbers)
+        print(len(match_numbers), len(no_match_numbers))
 
         if len(match_numbers) > 1 or len(no_match_numbers) > 1:
             self.fail(
