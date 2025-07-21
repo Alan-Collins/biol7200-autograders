55bc598404b7cf1aa3c6fabad775510626d14bdf
Alan Collins <alan.collins@bath.edu>
2025-07-21 10:49:52 -0400

    fix temp dir path in setupclass method

--- tests/test_spacers.py
+++ tests/test_spacers.py
@@ -88,7 +88,7 @@ class TestFiles(unittest.TestCase):
             return cls
         cls.submitted = True
         shutil.copy(SCRIPT_PATH, cls.dir)
-        cls.outfile = Path(f"{dir}/spacers.fna")
+        cls.outfile = Path(f"{cls.dir}/spacers.fna")
         Path(f"{cls.dir}/{SOLUTION_SCRIPT}").chmod(0o777)
         command = [
             f"./{SOLUTION_SCRIPT}",
