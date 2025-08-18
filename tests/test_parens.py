cb6ec88cae3b6c84a3766bbccc3c63b347637acd
Alan Collins <alan.collins@bath.edu>
2025-08-18 15:47:45 -0400

    add tests

--- tests/test_parens.py
+++ tests/test_parens.py
@@ -1,4 +1,8 @@
 import unittest
+from pathlib import Path
+import re
+import subprocess
+import ast
 
 from gradescope_utils.autograder_utils.decorators import weight, number, visibility
 from gradescope_utils.autograder_utils.files import check_submitted_files
@@ -16,7 +20,61 @@ POINT_NUM = PointCounter(0)
 class TestFiles(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        pass
+        cls.submission = Path(SCRIPT_PATH)
+        if not cls.submission.exists():
+            cls.submitted = False
+            return cls
+        cls.submission.chmod(0o777)
+        cls.submitted = True
+        cls.code = cls.submission.read_text()
+        # check shebang
+        pattern = re.compile(r"\#\![ ]?/usr/bin/env python3")
+        with open(SCRIPT_PATH) as f:
+            first_line = f.readline()
+        if re.match(pattern, first_line):
+            cls.shebang = True
+        else:
+            cls.shebang = False
+        try:
+            func_found = False
+            cls.func_bodies = []
+            indent = None
+            func_lines = []
+            cls.def_lines = []
+            cls.func_names = []
+            cls.baseline_code = []
+            for line in cls.code.splitlines():
+                if func_found:
+                    if not indent:
+                        # If the function starts with a de-indented comment can't get indent amount
+                        if len(line.split("#")[0].strip()) == 0:
+                            continue
+                        indent = re.match(r"^\s*", line)[0]
+                    # Add appropriately indented lines and blank lines
+                    # If the line is neither it indicates the end of the function body
+                    if line.startswith(indent) or len(line.split("#")[0].strip()) == 0:
+                        func_lines.append(line)
+                        continue
+                    else:
+                        cls.func_bodies.append("\n".join(func_lines))
+                        func_lines = []
+                        func_found = False
+                if line.startswith("def"):
+                    func_lines.append(line)
+                    cls.def_lines.append(line)
+                    cls.func_names.append(line.split()[1].split("(")[0])
+                    func_found = True
+                elif line.startswith("#") or line.startswith("import") or line.strip() == "":
+                    continue
+                else:
+                    cls.baseline_code.append(line)
+            cls.tree = ast.parse(cls.code)
+
+            cls.imported = True
+        except Exception as e:
+            cls.parse_exception = e
+            cls.imported = False
+            return cls
 
     @classmethod
     def tearDownClass(cls):
@@ -36,3 +94,206 @@ class TestFiles(unittest.TestCase):
             f'Missing script {SOLUTION_SCRIPT}, follow instructions carefully'
         )
         print(f'{SOLUTION_SCRIPT} script submitted successfully')
+
+
+    @weight(0)
+    @number(f"{Q_NUM}.{POINT_NUM.next()}")
+    @visibility("on_fail")
+    def test_shebang_present(self):
+        """Check script uses shebang"""
+        if not self.submitted:
+            self.fail("No script was submitted")
+        with open(SCRIPT_PATH) as f:
+            first_two = f.read()[:2]
+        if first_two != "#!":
+            self.fail(
+                f"Your scripts should always start with a shebang to ensure the correct program executes them."
+            )
+        print("Your script begins with a shebang.")
+
+    @weight(0)
+    @number(f"{Q_NUM}.{POINT_NUM.next()}")
+    @visibility("on_fail")
+    def test_shebang_correct(self):
+        """Check shebang correct"""
+        if not self.submitted:
+            self.fail("No script was submitted")
+        if not self.shebang:
+            self.fail(
+                f"Your script does not begin with a correct shebang."
+            )
+        print("Your script begins with a correct shebang.")
+
+    @weight(0)
+    @number(f"{Q_NUM}.{POINT_NUM.next()}")
+    @visibility("on_fail")
+    def test_parsed(self):
+        """Check your script could be parsed"""
+        if not self.submitted:
+            self.fail("No script was submitted")
+        if not self.imported:
+            self.fail(
+                "Couldn't parse your script due to the following error.\n"
+                "Please bring this issue to the attention of Dr. Collins as this may be an"
+                "issue with his autograder code..."
+                f"{self.parse_exception}"
+            )
+        for fname, f in zip(self.func_names, self.func_bodies):
+            try:
+                exec(f)
+            except Exception as e:
+                self.fail(
+                    f"There was an issue processing your function {fname}:\n"
+                    f"{e}\n"
+                    f"Function body:\n{f}"
+                )
+        
+        print(f"Script parsed successfully and {len(self.func_names)} functions were found.")
+    
+    # check base line code just functions and single call
+
+    @weight(1)
+    @number(f"{Q_NUM}.{POINT_NUM.next()}")
+    @visibility("visible")
+    def test_single_baseline_line(self):
+        """Check your script uses functions for all code except one call"""
+        if not self.submitted:
+            self.fail("No script was submitted")
+        if not self.imported:
+            self.fail(
+                "Couldn't parse your script."
+            )
+        if len(self.baseline_code) == 1:
+            print(
+                "Only one line of code found on the baseline:\n"
+                f"{self.baseline_code[0]}"  
+            )
+        else:
+            print("found multiple lines of code on the baseline:\n")
+            for line in self.baseline_code:
+                print(line)
+            self.fail("")
+
+
+    # run script and check triangles
+    @weight(5)
+    @number(f"{Q_NUM}.{POINT_NUM.next()}")
+    @visibility("visible")
+    def test_paired(self):
+        """Check your script works for paired parentheses"""
+        if not self.submitted:
+            self.fail("No script was submitted")
+        if not self.shebang:
+            self.fail(
+                f"Your script does not begin with a correct shebang."
+            )
+        tests = [
+            ("()", "PAIRED"),
+            ("()()()", "PAIRED"),
+            ("(()())", "PAIRED"),
+            ("((()))", "PAIRED"),
+            ("(()())", "PAIRED"),
+            ("((())())", "PAIRED")
+        ]
+        for input, expected in tests:
+            command = [self.submission, input]
+            result = subprocess.run(
+                command,
+                text=True,
+                capture_output=True
+            )
+            if result.returncode != 0:
+                self.fail(
+                    "Your script returned a non-zero exit code"
+                )
+            if result.stdout.strip() != expected:
+                self.fail(
+                    "Your script's output did not match the expected output for paired parentheses."
+                )
+        print("Your script correctly identified paired parentheses")
+        
+
+    @weight(5)
+    @number(f"{Q_NUM}.{POINT_NUM.next()}")
+    @visibility("visible")
+    def test_unpaired(self):
+        """Check your script works for paired parentheses"""
+        if not self.submitted:
+            self.fail("No script was submitted")
+        if not self.shebang:
+            self.fail(
+                f"Your script does not begin with a correct shebang."
+            )
+        tests = [
+            (")", "NOT PAIRED"),
+            (")(", "NOT PAIRED"),
+            ("()()(", "NOT PAIRED"),
+            (")((())", "NOT PAIRED"),
+            ("))((", "NOT PAIRED"),
+            ("())(", "NOT PAIRED")
+        ]
+        for input, expected in tests:
+            command = [self.submission, input]
+            result = subprocess.run(
+                command,
+                text=True,
+                capture_output=True
+            )
+            if result.returncode != 0:
+                self.fail(
+                    "Your script returned a non-zero exit code"
+                )
+            if result.stdout.strip() != expected:
+                self.fail(
+                    "Your script's output did not match the expected output for unpaired parentheses."
+                )
+        print("Your script correctly identified unpaired parentheses")
+
+
+    # interrogate docstrings
+    @weight(2)
+    @number(f"{Q_NUM}.{POINT_NUM.next()}")
+    @visibility("visible")
+    def test_docstrings_used(self):
+        """Check your functions have docstrings"""
+        if not self.submitted:
+            self.fail("No script was submitted")
+        if not self.shebang:
+            self.fail(
+                f"Your script does not begin with a correct shebang."
+            )
+        funcs = [n for n in self.tree.body if isinstance(n, (ast.FunctionDef))]
+        for f in funcs:
+            docstring = ast.get_docstring(f)
+            if not docstring.strip():
+                self.fail(
+                    f"No docstring found for your function {f.name}"
+                )
+        print("looks like docstrings were used. The quality of your docstrings will be assessed manually.")
+
+    # check type hints
+    @weight(2)
+    @number(f"{Q_NUM}.{POINT_NUM.next()}")
+    @visibility("visible")
+    def test_typehints_used(self):
+        """Check your functions have typehints"""
+        if not self.submitted:
+            self.fail("No script was submitted")
+        if not self.shebang:
+            self.fail(
+                f"Your script does not begin with a correct shebang."
+            )
+        funcs = [n for n in self.tree.body if isinstance(n, (ast.FunctionDef))]
+        for f in funcs:
+            if not f.returns:
+                self.fail(f"Missing return type annotation for {f.name}")
+            params = f.args.args
+            params.extend(f.args.posonlyargs)
+            params.extend(f.args.kwonlyargs)
+
+            for a in params:
+                if not a.annotation:
+                    self.fail(
+                        f"No annotation found for your function {f.name}'s param {a.arg}"
+                    )
+        print("looks like annotations were used. The quality of your annotations will be assessed manually.")
