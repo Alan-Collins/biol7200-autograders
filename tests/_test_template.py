4a3ccbf8d917924de7024a8fd86bc5464acb78c7
Alan Collins <alan.collins@bath.edu>
2025-08-30 16:32:40 -0400

    add boilerplate

--- tests/_test_template.py
+++ tests/_test_template.py
@@ -1,4 +1,7 @@
 import unittest
+from pathlib import Path
+import shutil
+import tempfile
 
 from gradescope_utils.autograder_utils.decorators import weight, number, visibility
 from gradescope_utils.autograder_utils.files import check_submitted_files
@@ -8,6 +11,7 @@ from utils import PointCounter
 SUBMISSION_PATH = "/autograder/submission/"
 SOLUTION_SCRIPT = "<script name>"
 SCRIPT_PATH = f"{SUBMISSION_PATH}{SOLUTION_SCRIPT}"
+DATA_DIR = "/autograder/biol7200-autograders/data/"
 
 Q_NUM = 4
 
@@ -16,11 +20,22 @@ POINT_NUM = PointCounter(0)
 class TestFiles(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        pass
+        cls.dir = Path(tempfile.mkdtemp())
+        cls.input_files = [
+        ]
+        for file in cls.input_files:
+            shutil.copy(f"{DATA_DIR}{file}", f"{cls.dir}/")
+        cls.submitted = True
+        cls.outfile = Path(f"{cls.dir}/out")
+        cls.submitted = True
+        
+        if not Path(SCRIPT_PATH).exists():
+            cls.submitted = False
+            return cls
 
     @classmethod
     def tearDownClass(cls):
-        pass
+        shutil.rmtree(cls.dir)
 
     @weight(0)
     @number(f"{Q_NUM}.{POINT_NUM.next()}")
@@ -36,3 +51,11 @@ class TestFiles(unittest.TestCase):
             f'Missing script {SOLUTION_SCRIPT}, follow instructions carefully'
         )
         print(f'{SOLUTION_SCRIPT} script submitted successfully')
+
+    @weight(0)
+    @number(f"{Q_NUM}.{POINT_NUM.next()}")
+    @visibility("on_fail")
+    def test_x(self):
+        """Check xyz"""
+        if not self.submitted:
+            self.fail("No script was submitted")
