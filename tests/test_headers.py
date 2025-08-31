baacf971454160f4a993ed1748887366e4f21601
Alan Collins <alan.collins@bath.edu>
2025-08-31 07:55:18 -0400

    type split instead of strip

--- tests/test_headers.py
+++ tests/test_headers.py
@@ -186,7 +186,7 @@ class TestFiles(unittest.TestCase):
                 f"The first character in a header line should be '>', not '{head[0]}'. "
                 "The '>' character is what indicates that a line is a header."
             )
-        if head.split() == ">contig.1":
+        if head.strip() == ">contig.1":
             self.fail("Your script did not change the headers of the test data")
         pattern = re.compile(r"^>sample_123.*contig")
         if not re.match(pattern, head):
@@ -203,7 +203,7 @@ class TestFiles(unittest.TestCase):
             self.fail("No script was submitted")
         with open(self._outfile) as f:
             head = f.readline()
-        if head.split() == ">contig.1":
+        if head.strip() == ">contig.1":
             self.fail("Your script did not change the headers of the test data")
         pattern = re.compile(r"^>sample_123.*contig")
         if not re.match(pattern, head):
