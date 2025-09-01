de87731fc93676e3da740210bf679cb44c702437
Alan Collins <alan.collins@bath.edu>
2025-09-01 19:38:59 -0400

    add mpl

--- gradescope_base/setup.sh
+++ gradescope_base/setup.sh
@@ -39,7 +39,7 @@ source "${HOME}/conda/etc/profile.d/conda.sh"
 source "${HOME}/conda/etc/profile.d/mamba.sh"
 mamba shell init
 source "${HOME}/.bashrc"
-mamba create -y -n biol7200 python pip
+mamba create -y -n biol7200 python pip matplotlib
 mamba activate biol7200
 
 # Clone autograder files
