18b65198d33bbe345449f339f6ecc92ebbe0183b
Alan Collins <alan.collins@bath.edu>
2025-09-03 16:42:57 -0400

    add plot file requirement

---
+++ tests/test_plot.py
@@ -0,0 +1,54 @@
+import unittest
+from pathlib import Path
+import shutil
+import tempfile
+
+from gradescope_utils.autograder_utils.decorators import weight, number, visibility
+from gradescope_utils.autograder_utils.files import check_submitted_files
+
+from utils import PointCounter
+
+SUBMISSION_PATH = "/autograder/submission/"
+SOLUTION_SCRIPT = "plot.py"
+SCRIPT_PATH = f"{SUBMISSION_PATH}{SOLUTION_SCRIPT}"
+PLOT_PATH = f"{SUBMISSION_PATH}plot.jpg"
+DATA_DIR = "/autograder/biol7200-autograders/data/"
+
+Q_NUM = 1
+
+POINT_NUM = PointCounter(0)
+
+class TestFiles(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.dir = Path(tempfile.mkdtemp())
+        cls.input_files = [
+        ]
+        for file in cls.input_files:
+            shutil.copy(f"{DATA_DIR}{file}", f"{cls.dir}/")
+        cls.submitted = True
+        cls.outfile = Path(f"{cls.dir}/out")
+        cls.submitted = True
+        
+        if not Path(SCRIPT_PATH).exists():
+            cls.submitted = False
+            return cls
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.dir)
+
+    @weight(0)
+    @number(f"{Q_NUM}.{POINT_NUM.next()}")
+    @visibility("on_fail")
+    def test_submitted_files(self):
+        """Check submitted files"""
+        missing_files = check_submitted_files([f'{SOLUTION_SCRIPT}', f'{PLOT_PATH}'])
+        for path in missing_files:
+            print(f'Missing {path}')
+        self.assertEqual(
+            len(missing_files),
+            0,
+            f'Missing script {SOLUTION_SCRIPT}, follow instructions carefully'
+        )
+        print(f'{SOLUTION_SCRIPT} script submitted successfully')
