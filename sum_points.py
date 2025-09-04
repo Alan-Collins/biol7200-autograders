dca551890b4f9949cea4e91fe904bc1a3a7494a5
Alan Collins <alan.collins@bath.edu>
2025-09-04 10:17:49 -0400

    add total

--- sum_points.py
+++ sum_points.py
@@ -3,10 +3,13 @@
 import os
 import re
 
+total = 0
 for file in os.listdir("tests/"):
     if not file.startswith("test"):
         continue
     with open(f"tests/{file}") as fin:
         vals = [int(i) for i in re.findall(r"@weight\((\d+)\)",fin.read())]
+        total += sum(vals)
     print(f"{file}\t{sum(vals)}")
+print(f"Total\t{total}")
     
