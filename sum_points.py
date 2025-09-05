47fba0044ac27cb7e5dc07d49ef34cc0006254e9
Alan Collins <alan.collins@bath.edu>
2025-09-05 15:46:32 -0400

    include partial_credit in count

--- sum_points.py
+++ sum_points.py
@@ -8,7 +8,7 @@ for file in os.listdir("tests/"):
     if not file.startswith("test"):
         continue
     with open(f"tests/{file}") as fin:
-        vals = [int(i) for i in re.findall(r"@weight\((\d+)\)",fin.read())]
+        vals = [int(i) for i in re.findall(r"@(?:weight|partial_credit)\((\d+)\)",fin.read())]
         total += sum(vals)
     print(f"{file}\t{sum(vals)}")
 print(f"Total\t{total}")
