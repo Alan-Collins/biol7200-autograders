ea3b2ea61109bae983681fc1f8ced54eea28bea8
Alan Collins <alan.collins@bath.edu>
2025-09-07 16:21:47 -0400

    copy files instead of symlink

--- tests/test_regexes.py
+++ tests/test_regexes.py
@@ -472,7 +472,7 @@ class TestFiles(unittest.TestCase):
         dir = mkdtemp()
         shutil.copytree(f"{DATA_DIR}find_data", f"{dir}/find_data")
         Path.mkdir(Path(f"{dir}/data"))
-        Path(f"{dir}/data/find_data").symlink_to(Path(f"{dir}/find_data"))
+        shutil.copytree(f"{DATA_DIR}find_data", f"{dir}/data/find_data")
         result = subprocess.run(
             command,
             shell=True,
@@ -516,7 +516,7 @@ class TestFiles(unittest.TestCase):
         dir = mkdtemp()
         shutil.copytree(f"{DATA_DIR}find_data", f"{dir}/find_data")
         Path.mkdir(Path(f"{dir}/data"))
-        Path(f"{dir}/data/find_data").symlink_to(Path(f"{dir}/find_data"))
+        shutil.copytree(f"{DATA_DIR}find_data", f"{dir}/data/find_data")
         result = subprocess.run(
             command,
             shell=True,
