2aef960411ec036b6dca4c58f97f1e4f8e149330
Alan Collins <alan.collins@bath.edu>
2025-09-09 08:20:28 -0400

    Revert "Revert "remove data dir""
    This reverts commit ea0fc227713f273b62130283c6cc11b90bb4c674.

--- tests/test_regexes.py
+++ tests/test_regexes.py
@@ -499,8 +499,6 @@ class TestFiles(unittest.TestCase):
         # set up temp dir and test command
         dir = mkdtemp()
         shutil.copytree(f"{DATA_DIR}find_data", f"{dir}/find_data")
-        Path.mkdir(Path(f"{dir}/data"))
-        Path(f"{dir}/data/find_data").symlink_to(Path(f"{dir}/find_data"))
         result = subprocess.run(
             command,
             shell=True,
@@ -555,8 +553,6 @@ class TestFiles(unittest.TestCase):
         # set up temp dir and test command
         dir = mkdtemp()
         shutil.copytree(f"{DATA_DIR}find_data", f"{dir}/find_data")
-        Path.mkdir(Path(f"{dir}/data"))
-        Path(f"{dir}/data/find_data").symlink_to(Path(f"{dir}/find_data"))
         result = subprocess.run(
             command,
             shell=True,
@@ -610,8 +606,6 @@ class TestFiles(unittest.TestCase):
         # set up temp dir and test command
         dir = mkdtemp()
         shutil.copytree(f"{DATA_DIR}find_data", f"{dir}/find_data")
-        Path.mkdir(Path(f"{dir}/data"))
-        shutil.copytree(f"{DATA_DIR}find_data", f"{dir}/data/find_data")
         gene_dir = Path(f"{dir}/genes")
         protein_dir = Path(f"{dir}/proteins")
         Path.mkdir(gene_dir)
