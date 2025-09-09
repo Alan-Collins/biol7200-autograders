c7529492a7f85a0ece8b204e10ffc05e76d6d631
Alan Collins <alan.collins@bath.edu>
2025-09-09 07:19:17 -0400

    remove data dir

--- tests/test_regexes.py
+++ tests/test_regexes.py
@@ -477,8 +477,6 @@ class TestFiles(unittest.TestCase):
         # set up temp dir and test command
         dir = mkdtemp()
         shutil.copytree(f"{DATA_DIR}find_data", f"{dir}/find_data")
-        Path.mkdir(Path(f"{dir}/data"))
-        Path(f"{dir}/data/find_data").symlink_to(Path(f"{dir}/find_data"))
         result = subprocess.run(
             command,
             shell=True,
@@ -529,8 +527,6 @@ class TestFiles(unittest.TestCase):
         # set up temp dir and test command
         dir = mkdtemp()
         shutil.copytree(f"{DATA_DIR}find_data", f"{dir}/find_data")
-        Path.mkdir(Path(f"{dir}/data"))
-        Path(f"{dir}/data/find_data").symlink_to(Path(f"{dir}/find_data"))
         result = subprocess.run(
             command,
             shell=True,
@@ -583,8 +579,6 @@ class TestFiles(unittest.TestCase):
         # set up temp dir and test command
         dir = mkdtemp()
         shutil.copytree(f"{DATA_DIR}find_data", f"{dir}/find_data")
-        Path.mkdir(Path(f"{dir}/data"))
-        shutil.copytree(f"{DATA_DIR}find_data", f"{dir}/data/find_data")
         gene_dir = Path(f"{dir}/genes")
         protein_dir = Path(f"{dir}/proteins")
         Path.mkdir(gene_dir)
