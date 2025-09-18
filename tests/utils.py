ec10188c488f04be83829fae2dbbd98abbe0386d
Alan Collins <alan.collins@bath.edu>
2025-09-18 14:48:49 -0400

    fix sequence slice

--- tests/utils.py
+++ tests/utils.py
@@ -301,7 +301,7 @@ class FastaSeq():
         for entry in fasta_str.split(">"):
             lines = [l for l in entry.split("\n") if l != ""]
             head = lines[0]
-            seq = "".join(lines[1])
+            seq = "".join(lines[1:])
             seqs.append(Seq(head, seq))
         return cls(seqs)
 
