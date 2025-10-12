ecfb0602f1537681505c1f3c0cb732a8028dbc8c
Alan Collins <alan.collins@bath.edu>
2025-10-12 13:02:38 -0400

    copytree to copy dir

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -31,7 +31,7 @@ class TestFiles(unittest.TestCase):
             "global_temperature"
         ]
         for file in cls.input_files:
-            shutil.copy(f"{DATA_DIR}{file}", f"{cls.dir}/")
+            shutil.copytree(f"{DATA_DIR}{file}", f"{cls.dir}/")
         cls.submitted = True
         cls.outfile = Path(f"{cls.dir}/plot.png")
        
