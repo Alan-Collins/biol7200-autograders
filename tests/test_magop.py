9cedfe559593cbb68668fe5f967d866d41924f6d
Alan Collins <alan.collins@bath.edu>
2025-11-03 13:18:38 -0500

    fix nw errors

--- tests/test_magop.py
+++ tests/test_magop.py
@@ -105,9 +105,9 @@ class TestFiles(unittest.TestCase):
         
         try: # run needleman_wunsch
             if not hasattr(magnumopus, "needleman_wunsch"):
-                raise("magnumopus has no function 'needleman_wunsch'")
+                raise AttributeError("magnumopus has no function 'needleman_wunsch'")
             if not inspect.isfunction(magnumopus.needleman_wunsch):
-                raise("magnumopus.needleman_wunsch is not a function")
+                raise AttributeError("magnumopus.needleman_wunsch is not a function")
             seq1 = "CTTCTCGTCGGTCTCGTGGTTCGGGAAC"
             seq2 = "CTTTCATCCACTTCGTTGCCCGGGAAC"
             cls.needleman_wunsch_result = magnumopus.needleman_wunsch(seq1, seq2, 1, -1, -1)
