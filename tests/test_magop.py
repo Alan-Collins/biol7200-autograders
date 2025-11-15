28986a06a5fe794622537671b41aba2ade284a7c
Alan Collins <alan.collins@bath.edu>
2025-11-15 16:34:05 -0500

    debugging

--- tests/test_magop.py
+++ tests/test_magop.py
@@ -208,13 +208,17 @@ class TestFiles(unittest.TestCase):
                 capture_output=True
             )
 
-            if result.stdout.strip() == "":
-                self.fail(
-                    f"Could not process your output tree. Your tree was\n{query_tree}\n"
-                    f"The error was {result.stderr}"
-                )
-            
-            scores = [float(i) for i in re.findall(r"\b(\d\.\d+)\b", result.stdout)]
+        if result.stdout.strip() == "":
+            self.fail(
+                f"Could not process your output tree. Your tree was\n{query_tree}\n"
+                f"The error was {result.stderr}"
+            )
+        
+        scores = [float(i) for i in re.findall(r"\b(\d\.\d+)\b", result.stdout)]
+
+        print(result.stdout)
+        print(result.stderr)
+        print(scores)
         
         return scores
 
