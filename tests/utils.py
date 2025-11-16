45c95157ca8c033fba621375deae02208b7bc2e0
Alan Collins <alan.collins@bath.edu>
2025-11-16 07:31:12 -0500

    fix condition

--- tests/utils.py
+++ tests/utils.py
@@ -319,7 +319,7 @@ class FastaSeq():
     
     
     def __eq__(self, other: "FastaSeq") -> bool:
-        if not isinstance(other, FastaSeq) or isinstance(other, Seq) or isinstance(other, str):
+        if not (isinstance(other, FastaSeq) or isinstance(other, Seq) or isinstance(other, str)):
             raise TypeError(f"== not supported between {self.__class__.__name__} and {other.__class__.__name__}")
         if isinstance(other, FastaSeq):
             return set(self.seqs) == set(other.seqs)
