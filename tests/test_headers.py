5cbd2a31809aee62dd3d7cdf726c1b71568a5508
Alan Collins <alan.collins@bath.edu>
2026-09-14 14:01:07 -0400

    fail on headers with only filename

--- tests/test_headers.py
+++ tests/test_headers.py
@@ -215,10 +215,15 @@ class TestFiles(unittest.TestCase):
             )
         if head.strip() == ">contig.1":
             self.fail("Your script did not change the headers of the test data")
-        pattern = re.compile(r"^>sample_123.*contig")
+        pattern = re.compile(r"^>sample_123.*")
         if not re.match(pattern, head):
             self.fail(
                 "Your header does not appear to include the test file name in the header"
+            )
+        pattern = re.compile(r"^>sample_123$")
+        if not re.match(pattern, head):
+            self.fail(
+                "Your headers is only the file name. How could multiple contigs be distinguished?"
             )
         print("The headers in your output file look like they are correctly formatted")
 
